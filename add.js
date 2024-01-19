a09445cd43fc5185417858bd5386cc3387f62cf6
mmwandera <mmwandera74@gmail.com>
2024-01-19 23:21:37 +0300

    module exports

---
+++ add.js
@@ -0,0 +1,6 @@
+const add = (a, b) => {
+    return a + b
+}
+
+// module.exports is an object
+module.exports = add;
---
+++ index.js
@@ -0,0 +1,9 @@
+const add = require('./add.js')
+
+console.log("mans just here init");
+const sum = add(1, 2)
+const sum2 = add(4, 3)
+
+console.log(sum)
+console.log(sum2)
+
