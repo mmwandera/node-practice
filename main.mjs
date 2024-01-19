45fa65b1656f686dfcdd24dee8a60d67d6d12d23
mmwandera <mmwandera74@gmail.com>
2024-01-20 00:48:13 +0300

    ES modules

---
+++ main.mjs
@@ -0,0 +1,13 @@
+// import math from './math-esm.mjs'
+
+// console.log(math.add(55, 10))
+// console.log(math.subtract(55, 10))
+
+// For Named Exports
+// import * as math from './math-esm.mjs'
+// const {add, subtract} = math
+
+import { add, subtract } from './math-esm.mjs'
+
+console.log(add(55, 10))
+console.log(subtract(55, 10))
---
+++ math-esm.mjs
@@ -0,0 +1,12 @@
+// Named export
+export const add = (a, b) => {
+    return a + b
+}
+
+// Named export
+export const subtract = (a, b) => {
+    return a - b
+}
+
+// Default export
+// export default {add, subtract};
