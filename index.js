e141388cf865deba8a52baf55e27fafd7e7a2290
mmwandera <mmwandera74@gmail.com>
2024-01-20 11:39:15 +0300

    callback pattern

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// const path = require('node:path');
-const path = require('path');
+// CALLBACKS
+// In js, functions are first class objects
+// Functions can be passed as arguments to other functions
+// A function can also be returned from another function
 
-console.log(path.join("hello", "world", "data.json")); // hello/world/data.json
-console.log(path.join("/hello", "world", "data.json")); // /hello/world/data.json
-console.log(path.join("/hello", "//world", "data.json")); // /hello/world/data.json
-console.log(path.join("/hello", "//world", "../data.json")); // /hello/data.json
-console.log(path.join(__dirname, "data.json")); //gives absolute path to data.json
+// A function passed as an argument to another function is called a callback function
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
 
-console.log(path.resolve("hello", "world", "data.json"));
-console.log(path.resolve("/hello", "world", "data.json"));
-console.log(path.resolve("/hello", "//world", "data.json"));
-console.log(path.resolve("/hello", "//world", "../data.json")); 
-console.log(path.resolve(__dirname, "data.json")); 
+function greetMark(greetFn) {
+    const name = 'Mark';
+    greetFn('Mark');
+}
 
-// console.log(__dirname)
-console.log(__filename)
-
-console.log(path.basename(__dirname))
-console.log(path.basename(__filename))
-
-console.log(path.extname(__dirname))
-console.log(path.extname(__filename))
-
-console.log(path.parse(__filename))
-
-console.log(path.format(path.parse(__filename)))
-
-console.log(path.isAbsolute(__filename)) // Returns true if the path is absolute
-console.log(path.isAbsolute('./data.json')) // Returns false if the path is absolute
-
+greetMark(greet); // "Hello Mark"
