e0cb42f42c2578ebf5e11d2ec2ed76ec713b6459
mmwandera <mmwandera74@gmail.com>
2024-01-20 11:26:53 +0300

    path module git status

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const data = require('./data.json');
+// const path = require('node:path');
+const path = require('path');
 
-console.log(data)
-console.log(data.name)
-console.log(data.address.street)
-console.log(data.address.city)
+console.log(path.join("hello", "world", "data.json")); // hello/world/data.json
+console.log(path.join("/hello", "world", "data.json")); // /hello/world/data.json
+console.log(path.join("/hello", "//world", "data.json")); // /hello/world/data.json
+console.log(path.join("/hello", "//world", "../data.json")); // /hello/data.json
+console.log(path.join(__dirname, "data.json")); //gives absolute path to data.json
 
-// node --watch index.js
+console.log(path.resolve("hello", "world", "data.json"));
+console.log(path.resolve("/hello", "world", "data.json"));
+console.log(path.resolve("/hello", "//world", "data.json"));
+console.log(path.resolve("/hello", "//world", "../data.json")); 
+console.log(path.resolve(__dirname, "data.json")); 
+
+// console.log(__dirname)
+console.log(__filename)
+
+console.log(path.basename(__dirname))
+console.log(path.basename(__filename))
+
+console.log(path.extname(__dirname))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
+
+console.log(path.format(path.parse(__filename)))
+
+console.log(path.isAbsolute(__filename)) // Returns true if the path is absolute
+console.log(path.isAbsolute('./data.json')) // Returns false if the path is absolute
+
