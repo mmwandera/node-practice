20cb1f84a1ee710804cf2bc2c730ab2a67c3b1c6
mmwandera <mmwandera74@gmail.com>
2024-01-25 22:50:13 +0300

    streams and buffers

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-const PizzaShop = require('./pizza-shop.js');
-const DrinkMachine = require('./drink-machine.js')
+const buffer = new Buffer.from("mans just here init")
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
+buffer.write("as i said, mans just here init")
 
-pizzaShop.on("order", (size, toppings) => {
-    console.log(`Order for ${size} pizza with ${toppings} toppings.`)
-    drinkMachine.serveDrink("large")
-})
-
-pizzaShop.order("large", "mushroom");
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require('node:events');
-
-// // Instantiating emitter object from EventEmitter class
-// const emitter = new EventEmitter();
-
-// // Responding to "order-pizza" event
-// emitter.on("order-pizza", (size, toppings) => {
-//     console.log(`Order for ${size} pizza with ${toppings} toppings.`)
-// })
-
-// emitter.on("order-pizza", (size) => {
-//     if (size === "large") {
-//         console.log("Serving complimentary drink")
-//     }
-// })
-
-// // Using the emitter object, we can emit events
-// emitter.emit("order-pizza", "large", "mushroom")
+console.log(buffer)
+console.log(buffer.toJSON()) // 
+console.log(buffer.toString())
