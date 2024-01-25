729b34272b03c1d554fd7a05f02c412780f8d722
mmwandera <mmwandera74@gmail.com>
2024-01-25 23:31:54 +0300

    fs promise module

--- index.js
+++ index.js
@@ -1,47 +1,76 @@
-const fs = require("node:fs")
+const fs = require("node:fs/promises")
 
-// WRITING FILES
 console.log("First")
 
-// Reading files synchronously
-const fileContents = fs.readFileSync("file.txt", "utf8")
-console.log(fileContents)
-
+fs.readFile("file.txt", "utf8")
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 console.log("Second")
 
-// Reading files asynchronously
-fs.readFile("file.txt", "utf8", (err, data) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(data)
-    }
-})
+// async function readFile(){
+//     try{
+//         const data = await fs.readFile("file.txt", "utf8")
+//         console.log(data)
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 
-console.log("Third")
+// readFile()
 
-// WRITING FILES
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 
-// Writing files synchronously
-fs.writeFileSync("./greet.txt", "whagwan bruv")
 
-// Writing files asynchronously
-// If the file does not exist it will be created
-// By default writeFile will overwrite existing files if file exists
-// fs.writeFile("./greet.txt", "in a bit bruv", (err) => {
+// const fs = require("node:fs")
+
+// // WRITING FILES
+// console.log("First")
+
+// // Reading files synchronously
+// const fileContents = fs.readFileSync("file.txt", "utf8")
+// console.log(fileContents)
+
+
+// console.log("Second")
+
+// // Reading files asynchronously
+// fs.readFile("file.txt", "utf8", (err, data) => {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(data)
+//     }
+// })
+
+// console.log("Third")
+
+// // WRITING FILES
+
+// // Writing files synchronously
+// fs.writeFileSync("./greet.txt", "whagwan bruv")
+
+// // Writing files asynchronously
+// // If the file does not exist it will be created
+// // By default writeFile will overwrite existing files if file exists
+// // fs.writeFile("./greet.txt", "in a bit bruv", (err) => {
+// //     if(err){
+// //         console.log(err)
+// //     }else{
+// //         console.log("File written successfully")
+// //     }
+// // })
+
+// // To append to a file
+// fs.writeFile("./greet.txt", ". you good fam?", {flag: "a"}, (err) => {
 //     if(err){
 //         console.log(err)
 //     }else{
 //         console.log("File written successfully")
 //     }
 // })
-
-// To append to a file
-fs.writeFile("./greet.txt", ". you good fam?", {flag: "a"}, (err) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log("File written successfully")
-    }
-})
