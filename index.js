9c1763f09c4230dae357041c6d5c0fbeb9919e76
mmwandera <mmwandera74@gmail.com>
2024-01-26 21:15:21 +0300

    HTTP routing

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const http = require("http")
 const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-    const name = "Mariko"
-    res.writeHead(200, {"Content-Type": "text/html"});
-    let html = fs.readFileSync("./index.html", "utf-8");
-    html = html.replace("{{name}}", name)
-    res.end(html);
+    // req.method => GET, POST, PUT, PATCH, DELETE
+    if(req.url === "/" ){
+        res.writeHead(200, { "Content-Type": "text/plain" })
+        res.end("Home Page")
+    } else if(req.url === "/about"){
+        res.writeHead(200, { "Content-Type": "text/plain" })
+        res.end("About Page")
+    } else if(req.url === "/api"){
+        res.writeHead(200, { "Content-Type": "application/json" })
+        const obj = { firstName: "Bruce", lastName: "Wayne" }
+        res.end(JSON.stringify(obj))
+    } else{
+        res.writeHead(404)
+        res.end("Page not found")
+    }
 })
 
 server.listen(3000, () => {
     console.log("Server running on port 3000")
 })
 
-// const server = http.createServer((req, res) => {
-
-//     res.writeHead(200, {"Content-Type": "text/html"});
-//     res.end("<h1>mans just here init</h1>");
-// })
-
-// server.listen(3000, () => {
-//     console.log("Server running on port 3000")
-// })
