8e71e6f7a2d7d01980c4fa0b0931cd3e8c40b27e
mmwandera <mmwandera74@gmail.com>
2024-01-26 17:46:22 +0300

    HTML response

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const http = require("http")
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-    const superHero = {
-        firstName: "Clark",
-        lastName: "Kent"
-    }
-    
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(superHero));
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(__dirname + "/index.html").pipe(res)
+    // const html = fs.readFileSync("./index.html", "utf-8");
+    // res.end(html);
 })
 
 server.listen(3000, () => {
     console.log("Server running on port 3000")
 })
+
+// const server = http.createServer((req, res) => {
+
+//     res.writeHead(200, {"Content-Type": "text/html"});
+//     res.end("<h1>mans just here init</h1>");
+// })
+
+// server.listen(3000, () => {
+//     console.log("Server running on port 3000")
+// })
