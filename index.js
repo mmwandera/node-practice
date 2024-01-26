4370ba70802bf80192adc66318a89aba87696bea
mmwandera <mmwandera74@gmail.com>
2024-01-26 22:30:12 +0300

    microtask queues

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const http = require("http")
-const fs = require("fs")
-
-const server = http.createServer((req, res) => {
-    // req.method => GET, POST, PUT, PATCH, DELETE
-    if(req.url === "/" ){
-        res.writeHead(200, { "Content-Type": "text/plain" })
-        res.end("Home Page")
-    } else if(req.url === "/about"){
-        res.writeHead(200, { "Content-Type": "text/plain" })
-        res.end("About Page")
-    } else if(req.url === "/api"){
-        res.writeHead(200, { "Content-Type": "application/json" })
-        const obj = { firstName: "Bruce", lastName: "Wayne" }
-        res.end(JSON.stringify(obj))
-    } else{
-        res.writeHead(404)
-        res.end("Page not found")
-    }
+Promise.resolve().then(() => {
+    console.log('I am promise')
+})
+process.nextTick(() => {
+    console.log('I am next tick')
 })
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
-
+// I am next tick
+// I am promise
