b0a05b77455d6069f0cdc88b121680874bf3b483
mmwandera <mmwandera74@gmail.com>
2024-01-26 22:37:23 +0300

    timer queues

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+setTimeout(() => {
+    console.log("I am timeout")
+}, 3000)
+
 Promise.resolve().then(() => {
     console.log('I am promise')
 })
@@ -5,5 +9,7 @@ process.nextTick(() => {
     console.log('I am next tick')
 })
 
+// Callbacks in the microtask queues are executed before callbacks in the timer queue
 // I am next tick
 // I am promise
+// I am timeout
