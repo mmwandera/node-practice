f6ea18bf7598dfbf13e08e55fdae8672844d4f7d
mmwandera <mmwandera74@gmail.com>
2024-01-27 10:31:38 +0300

    I/O polling

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ fs.readFile(__filename, () => {
     console.log('I am read file')
 })
 
+process.nextTick(() => {
+    console.log("I am next tick")
+})
+
+Promise.resolve().then(() => {
+    console.log("I am promise")
+})
+
 setTimeout(() => {
     console.log("I am timeout")
 }, 0)
 
-// When running setTimeout with a delay of 0ms and an I/O sync method, the order of execution can never be guaranteed
+setImmediate(() => {
+    console.log("I am immediate")
+})
+
+for (let i =0; i<2000000000; i++) {}
+
+// "I am immediate" is logged before "I am timeout" which is strange cause I/O queue is before check queue
